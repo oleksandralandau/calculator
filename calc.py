bdf0fb84345bfe28a49d89dec69d9fd259d8439d
Oleksandra Landau <landausa23@gmail.com>
2023-03-07 18:12:25 +0100

    new commit

--- calc.py
+++ calc.py
@@ -2,14 +2,16 @@ oper = input("Enter operation using an appropriate number: 1 Addition, 2 Subtrac
 x = input("Enter first component: ")
 y = input("Enter second component: ")
 if oper == '1':
-    print("Adding ", float(x), "and", float(y))
-    print("Result: ", float(x) + float(y))
+    a = input("Enter one more component or 0: ")
+    print("Adding ", float(x), "and", float(y), "and", float(a))
+    print("Result: ", float(x) + float(y) + float(a))
 elif oper == '2':
     print("Subtracting ", float(x), "and", float(y))
     print("Result: ", float(x) - float(y))
 elif oper == '3':
-    print("Multiplicating ", float(x), "and", float(y))
-    print("Result: ", float(x) * float(y))
+    m = input("Enter one more component or 1: ")
+    print("Multiplicating ", float(x), "and", float(y), "and", float(m))
+    print("Result: ", float(x) * float(y) * float(m))
 elif oper == '4':
     print("Dividing ", float(x), "and", float(y))
     print("Result: ", float(x) / float(y))
