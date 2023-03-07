494fee25c11c1325aef60fa0f25f68ee36d78ed8
Oleksandra Landau <landausa23@gmail.com>
2023-03-07 18:57:18 +0100

    new commit

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
+# calculator
 oper = input("Enter operation using an appropriate number: 1 Addition, 2 Subtraction, 3 Multiplication, 4 Division: ")
 x = input("Enter first component: ")
 y = input("Enter second component: ")
+
+# didn't get where to use logging
 if oper == '1':
     a = input("Enter one more component or 0: ")
     print("Adding ", float(x), "and", float(y), "and", float(a))
