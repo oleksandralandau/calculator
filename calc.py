de1b2fa2a6ab45a6ca7d0b0617c6e099db18882c
Oleksandra Landau <landausa23@gmail.com>
2023-03-23 11:54:19 +0200

    update 23.03.2023

--- calc.py
+++ calc.py
@@ -1,20 +1,22 @@
 # calculator
+import logging
+logging.basicConfig(level=logging.DEBUG)
 oper = input("Enter operation using an appropriate number: 1 Addition, 2 Subtraction, 3 Multiplication, 4 Division: ")
 x = input("Enter first component: ")
 y = input("Enter second component: ")
 
-# didn't get where to use logging
+# finally get where to use logging ;)
 if oper == '1':
     a = input("Enter one more component or 0: ")
-    print("Adding ", float(x), "and", float(y), "and", float(a))
+    logging.info(f"Adding {float(x)} and {float(y)} and {float(a)}")
     print("Result: ", float(x) + float(y) + float(a))
 elif oper == '2':
-    print("Subtracting ", float(x), "and", float(y))
+    logging.info(f"Subtracting {float(x)} and {float(y)}")
     print("Result: ", float(x) - float(y))
 elif oper == '3':
     m = input("Enter one more component or 1: ")
-    print("Multiplicating ", float(x), "and", float(y), "and", float(m))
+    logging.info(f"Multiplicating {float(x)} and {float(y)} and {float(m)}")
     print("Result: ", float(x) * float(y) * float(m))
 elif oper == '4':
-    print("Dividing ", float(x), "and", float(y))
+    logging.info(f"Dividing {float(x)} and {float(y)}")
     print("Result: ", float(x) / float(y))
